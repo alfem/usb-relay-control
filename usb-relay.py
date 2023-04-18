1955ebb8f81d63bc18d83afa75d7298899887aa6
Alfonso De Cala Bravo <alfonso.cala@juntadeandalucia.es>
2023-04-18 12:54:56 +0200

    Added parameters for device, relay number and command

--- usb-relay.py
+++ usb-relay.py
@@ -2,14 +2,38 @@
 # Control USB relays board from CFsunbird
 # Author: Alfonso de Cala <alfonso@el-magnifico.org>
 
-DEVICE='/dev/hidraw1'
+# Device file name usually is /dev/hidrawN
+# Your user should have write permission to this file (or run the script as root)
+
+import sys
+
+# Get parameters from command line
+if len(sys.argv) != 4:
+    print("Usage: python usb-relay.py <device file name> <relay number> <on/off>")
+    sys.exit(1)
+
+DEVICE = sys.argv[1]
+RELAY = int(sys.argv[2])
+COMMAND = sys.argv[3]
+
+# Parameter checking
+if RELAY < 1 or RELAY > 4:
+    print("Relay number must be between 1 and 4")
+    sys.exit(1)
+
+if COMMAND == "on":
+  command = 0
+elif COMMAND == "off":
+   command = 1
+else:   
+    print("Command must be 'on' or 'off'")
+    sys.exit(1)
 
 prefix=int('A1',16)
-relay=1
-command=0
+relay=int(RELAY)
 checksum=prefix+relay+command
 
-buffer=bytearray([prefix,relay,command,checksum])
+buffer=bytearray([prefix,RELAY,command,checksum])
 
 with open(DEVICE, 'wb') as f:
   f.write(buffer)
